86d92715e03e0cd982f321967df94e362a283590
fainashalts <faina@trufflesuite.com>
2021-07-14 16:37:16 -0700

    Add gasPrice and gas now that optimistic Kovan costs gas

--- test/simplestorage.js
+++ test/simplestorage.js
@@ -5,12 +5,12 @@ const SimpleStorage = artifacts.require("SimpleStorage");
 contract("SimpleStorage", accounts => {
 
   beforeEach(async () => {
-    simpleStorageInstance = await SimpleStorage.new({ gasPrice: 0 });
+    simpleStorageInstance = await SimpleStorage.deployed({ gasPrice: 15000000 });
   })
 
   it("...should store the value 89.", async () => {
     // Set value of 89
-    const tx = await simpleStorageInstance.set(89, { from: accounts[0], gasPrice: 0 });
+    const tx = await simpleStorageInstance.set(89, { from: accounts[0], gasPrice: 15000000 });
 
     // Get stored value
     const storedData = await simpleStorageInstance.get.call();
